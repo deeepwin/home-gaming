8c80440eb6d9624eca9ae2d3c93e0f5444658623
deeepwin <deeepwin@gmail.com>
2023-03-05 18:05:33 +0100

    throttle works, with button control

--- boating/code.py
+++ boating/code.py
@@ -1,14 +1,15 @@
-#import board support libraries, including HID.
+# import board support libraries, including HID.
 import board
 import analogio
 import usb_hid
 
 from time import sleep
 
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keycode import Keycode
 from hid_gamepad import Gamepad
 
-
-#local functions
+# local functions
 def range_map(x, in_min, in_max, out_min, out_max):
     return (x - in_min) * (out_max - out_min) // (in_max - in_min) + out_min
 
@@ -16,6 +17,17 @@ def read_analog():
     x = ax.value - x_offset
     y = ay.value - y_offset
     return x, y
+
+# key board configuration
+keyboard = Keyboard(usb_hid.devices)
+
+throttle_value = 0
+
+def press_button(key):
+    keyboard.press(key) 
+    sleep(0.1)
+    keyboard.release(key)
+    sleep(0.1)
 
 # game pad configuration (x/y values)
 gp = Gamepad(usb_hid.devices)
@@ -32,6 +44,10 @@ x_offset = y_offset = 0
 # calibrate to zero on startup
 x_offset, y_offset = read_analog()
 
+# initalize filters
+y_filter = y_offset
+x_filter = x_offset
+
 # find max range after calibration
 x_max_offset = abs(32768 - x_offset)
 x_max_range = 32768 + x_max_offset + 200
@@ -43,16 +59,37 @@ while True:
     # read current analog value
     x_value, y_value = read_analog()
     
+    # filter signal
+    y_value = y_filter * 0.8 + y_value * 0.2
+    y_filter = y_value
+    
+    x_value = x_filter * 0.9 + x_value * 0.1
+    x_filter = x_value
+    
     # update values directly in joystick
-    x = range_map(x_value, -x_max_range, x_max_range, -127, 127)
-    y = range_map(y_value, -y_max_range, y_max_range, -127, 127)
+    x = range_map(int(x_value), -x_max_range, x_max_range, -127, 127)
+    y = range_map(int(y_value), -y_max_range, y_max_range, -127, 127)
         
     gp.move_joysticks(
         x=x,
         y=y,
     )
     
-    print("{:.1f} {:.1f}".format(x, y))
+    # handle throttle control
+    throttle_pos = int(round((10.0 / 15000.0) * y_value))
+
+    if throttle_pos == 0 and throttle_value != 0:
+        press_button(Keycode.C)
+        throttle_value = 0
+    else:
+        if throttle_pos > throttle_value:
+            press_button(Keycode.S)
+            throttle_value += 1
+            
+        if throttle_pos < throttle_value:
+            press_button(Keycode.W)
+            throttle_value -= 1
     
-    sleep(0.05)
-    
+    #print("{:.1f} {:.1f}".format(throttle_pos, y_value))
+    #print("{:.1f} {:.1f}".format(x, y))
+ 
